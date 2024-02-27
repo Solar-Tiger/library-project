d9f819d78ffbd9ca6e96766b7c4e779d45f2d576
David Singleton <davidjsingleton18@gmail.com>
2024-02-27 10:58:48 -0800

    Update 'addBook' to 'newBook' to avoid data attribute clashing

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,10 +1,10 @@
-const addBook = document.querySelector('[data-add-book]');
+const newBook = document.querySelector('[data-new-book]');
 const removeBook = document.querySelector('[data-remove-book]');
 const addBookModal = document.querySelector('[data-add-book-modal]');
 const cancelBookModal = document.querySelector('[data-cancel-book-modal]');
 
-console.log(addBook);
-addBook.addEventListener('click', () => {
+console.log(newBook);
+newBook.addEventListener('click', () => {
   addBookModal.showModal();
 });
 
