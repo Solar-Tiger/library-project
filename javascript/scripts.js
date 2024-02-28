f916d08e19e6434bcaff38102cd47ae5bb9453a6
David Singleton <davidjsingleton18@gmail.com>
2024-02-28 11:04:41 -0800

    Add small example of using addBook to gather and use user information

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,8 +1,12 @@
+// buttons for event listener
+
 const newBook = document.querySelector('[data-new-book]');
 const addBook = document.querySelector('[data-add-book]');
 const removeBook = document.querySelector('[data-remove-book]');
 const cancelBookModal = document.querySelector('[data-cancel-book-modal]');
 const addBookModal = document.querySelector('[data-add-book-modal]');
+
+// user info to use for book constructor
 
 const bookTitle = document.querySelector('[data-book-title');
 const bookAuthor = document.querySelector('[data-book-author');
@@ -10,7 +14,6 @@ const bookPageNumber = document.querySelector('[data-book-page-number');
 const bookRead = document.querySelector('[data-book-read');
 const bookNotRead = document.querySelector('[data-book-not-read');
 
-console.log(newBook);
 newBook.addEventListener('click', () => {
   addBookModal.showModal();
 });
@@ -21,6 +24,16 @@ cancelBookModal.addEventListener('click', () => {
 
 addBook.addEventListener('click', (e) => {
   e.preventDefault();
+
+  const book = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPageNumber.value
+  );
+
+  console.dir(bookRead);
+
+  console.log(book);
 });
 
 function Book(title, author, numberOfPages, readOrNotRead) {
@@ -30,4 +43,4 @@ function Book(title, author, numberOfPages, readOrNotRead) {
   this.readOrNotRead = readOrNotRead;
 }
 
-console.dir(addBook);
+console.dir(bookTitle);
