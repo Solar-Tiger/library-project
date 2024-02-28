f2bf1982ca2fb065b06f5049ea2b07f54f553a1a
David Singleton <davidjsingleton18@gmail.com>
2024-02-28 10:57:13 -0800

    Add way to get user information and use it for the book constructor

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,7 +1,14 @@
 const newBook = document.querySelector('[data-new-book]');
+const addBook = document.querySelector('[data-add-book]');
 const removeBook = document.querySelector('[data-remove-book]');
-const addBookModal = document.querySelector('[data-add-book-modal]');
 const cancelBookModal = document.querySelector('[data-cancel-book-modal]');
+const addBookModal = document.querySelector('[data-add-book-modal]');
+
+const bookTitle = document.querySelector('[data-book-title');
+const bookAuthor = document.querySelector('[data-book-author');
+const bookPageNumber = document.querySelector('[data-book-page-number');
+const bookRead = document.querySelector('[data-book-read');
+const bookNotRead = document.querySelector('[data-book-not-read');
 
 console.log(newBook);
 newBook.addEventListener('click', () => {
@@ -11,3 +18,16 @@ newBook.addEventListener('click', () => {
 cancelBookModal.addEventListener('click', () => {
   addBookModal.close();
 });
+
+addBook.addEventListener('click', (e) => {
+  e.preventDefault();
+});
+
+function Book(title, author, numberOfPages, readOrNotRead) {
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.readOrNotRead = readOrNotRead;
+}
+
+console.dir(addBook);
